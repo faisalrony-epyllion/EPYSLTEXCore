bca9a9ac6321cfee55f075ac46350cbde8ff238f
Nishadur Rahman Evan <nishad@epylliongroup.com>
2024-11-20 10:32:08 +0600

    menu_fixed

--- EPYSLTEXCore.API/wwwroot/Scripts/common/app.js
+++ EPYSLTEXCore.API/wwwroot/Scripts/common/app.js
@@ -471,17 +471,17 @@ function generateMenu(menuList) {
             var updatednavigateUrl = item.navigateUrl.replace(/\//g, '_');
 
             if (navProperties[1] == 'notfoundpartial') {
-                template += '<li><a href="#!" data-navurl-name="' + updatednavigateUrl + '" data-controller-name="' + controllerName + '" data-action-name="' + actionName +'" data-page-name="' + item.pageName + '" data-menu-id="' + item.menuId + '" data-page-type="NF"><i class="fa fa-circle-o"></i> <span>' + item.menuCaption + '</span></a></li>';
+                template += '<li class="nav-item"><a href="#!" class="nav-link" data-navurl-name="' + updatednavigateUrl + '" data-controller-name="' + controllerName + '" data-action-name="' + actionName + '" data-page-name="' + item.pageName + '" data-menu-id="' + item.menuId + '" data-page-type="NF"><i class="nav-icon fa fa-circle-o"></i> <p>' + item.menuCaption + '</p></a></li>';
             }
             else if (item.useCommonInterface) {
-                template += '<li><a href="#!" data-navurl-name="' + updatednavigateUrl + '" data-controller-name="' + navProperties[0] + '" data-action-name="' + navProperties[1] + '" data-page-name="' + item.pageName + '" data-menu-id="' + item.menuId + '" data-page-type="CI"><i class="fa fa-circle-o"></i> <span>' + item.menuCaption + '</span></a></li>';
+                template += '<li class="nav-item"><a href="#!" class="nav-link" data-navurl-name="' + updatednavigateUrl + '" data-controller-name="' + navProperties[0] + '" data-action-name="' + navProperties[1] + '" data-page-name="' + item.pageName + '" data-menu-id="' + item.menuId + '" data-page-type="CI"><i class="nav-icon fa fa-circle-o"></i> <p>' + item.menuCaption + '</p></a></li>';
             }
             else if (item.pageName == 'ReportViewer') {
                 var path = rootPath + '/reports/index';
-                template += '<li><a href="' + path + '" target="_blank" data-page-type="Report"><i class="fa fa-circle-o"></i> <span>' + item.menuCaption + '</span></a></li>';
+                template += '<li class="nav-item"><a class="nav-link" href="' + path + '" target="_blank" data-page-type="Report"><i class="nav-icon fa fa-circle-o"></i> <p>' + item.menuCaption + '</p></a></li>';
             }
             else {
-                template += '<li><a href="#!" data-navurl-name="' + updatednavigateUrl + '"  data-controller-name="' + controllerName + '" data-action-name="' + actionName +'" data-table-id="' + navProperties[2] + '" data-page-name="' + item.pageName + '" data-menu-id="' + item.menuId + '"><i class="fa fa-circle-o"></i> <span>' + item.menuCaption + '</span></a></li>';
+                template += '<li class="nav-item"><a class="nav-link" href="#!" data-navurl-name="' + updatednavigateUrl + '"  data-controller-name="' + controllerName + '" data-action-name="' + actionName + '" data-table-id="' + navProperties[2] + '" data-page-name="' + item.pageName + '" data-menu-id="' + item.menuId + '"><i class="nav-icon fa fa-circle-o"></i> <p>' + item.menuCaption + '</p></a></li>';
             }
        
             activeMenu = false;
@@ -491,26 +491,28 @@ function generateMenu(menuList) {
   
             activeMenu = item.menuId == 509 ? true : false;
             var active = activeMenu ? "active" : "";
-            template += '<li class="treeview ' + active + '">';
-            template += '<a href="#">'
-                + '<i class="fa fa-circle"></i><span>' + item.menuCaption + '</span>'
-                + '<span class="pull-right-container">'
-                + '<i class="fa fa-angle-left pull-right"></i>'
-                + '</span>'
+            template += '<li class="nav-item' + active + '">';
+            template += '<a href="#" class="nav-link">'
+                + '<i class="nav-icon fa fa-circle"></i><p>' + item.menuCaption + '<i class="right fa fa-angle-left"></i></p>'
+            //    + '<p class="pull-right-container">'
+           
+              //  + '</p>'
                 + '</a>';
-            template += '<ul class="treeview-menu">';
+            template += '<ul class="nav nav-treeview">';
             activeMenu = false;
         }
 
         generateMenu(item.childs);
     
         template += '</ul></li>';
+
+        
     });
 }
 
 // #region Constants
 var constants = Object.freeze({
-    APPLICATION_ID: 11,
+    APPLICATION_ID: 8,
     LOAD_ERROR_MESSAGE: "An error occured in fetching your data",
     SUCCESS_MESSAGE: "Your record saved successfully!",
     PROPOSE_SUCCESSFULLY: "Your record has been sent for approval!",

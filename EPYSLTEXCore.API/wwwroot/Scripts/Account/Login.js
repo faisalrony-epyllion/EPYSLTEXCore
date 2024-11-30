aed7296ade64ee11b20138fda436bf8abad34091
Nishadur Rahman Evan <nishad@epylliongroup.com>
2024-11-30 08:57:43 +0600

    login-updated

--- EPYSLTEXCore.API/wwwroot/Scripts/Account/Login.js
+++ EPYSLTEXCore.API/wwwroot/Scripts/Account/Login.js
@@ -32,10 +32,16 @@ function changeForm(change) {
     if (change == 0) {
 
         $("#forgot-password").show();
+        $("#forgot-span").show();
+
         $("#sign-in").hide();
+        $("#sign-in-span").hide();
     }
     else {
         $("#sign-in").show();
+        $("#sign-in-span").show();
+
+        $("#forgot-span").hide();
         $("#forgot-password").hide();
     }
 

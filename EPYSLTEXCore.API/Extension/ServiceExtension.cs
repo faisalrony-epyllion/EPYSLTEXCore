03cf40f3f230b371669873a6d37cf642915f9215
Nishadur Rahman Evan <nishad@epylliongroup.com>
2024-12-04 09:49:31 +0600

    updated

--- EPYSLTEXCore.API/Extension/ServiceExtension.cs
+++ EPYSLTEXCore.API/Extension/ServiceExtension.cs
@@ -17,31 +17,30 @@ namespace EPYSLTEXCore.API.Extension
 
 
             service.AddScoped(typeof(IDapperCRUDService<>), typeof(DapperCRUDService<>));
-            //service.AddTransient<IMenuService, MenuService>();
-            //service.AddTransient<IMenuDAL, MenuDAL>();
-            //service.AddTransient<IUserService, UserService>();
+            service.AddTransient<IMenuService, MenuService>();
+            service.AddTransient<IUserService, UserService>();
             service.AddTransient<ITokenBuilder, TokenBuilder>();
             service.AddTransient<IDeSerializeJwtToken, DeSerializeJwtToken>();
-            //service.AddTransient<ICommonInterfaceService, CommonInterfaceService>();
-            //service.AddTransient<ISelect2Service, Select2Service>();
-            //service.AddTransient<ICommonHelpers, CommonHelpers>();
-            //service.AddTransient<IYarnProductSetupService, YarnProductSetupService>();
-            //service.AddTransient<IReportAPISetupService, ReportAPISetupService>();
+            service.AddTransient<ICommonInterfaceService, CommonInterfaceService>();
+            service.AddTransient<ISelect2Service, Select2Service>();
+            service.AddTransient<ICommonHelpers, CommonHelpers>();
+            service.AddTransient<IYarnProductSetupService, YarnProductSetupService>();
+            service.AddTransient<IReportAPISetupService, ReportAPISetupService>();
             //service.AddTransient<ISignatureService, SignatureService>();
 
-            foreach (var type in typeof(CommonHelperService).Assembly.GetTypes())
-            {
-                if (type.Name.EndsWith("Service") && type.IsClass && !type.IsAbstract)
-                {
-                    // Get all interfaces implemented by the type
-                    var interfaces = type.GetInterfaces();
-                    foreach (var @interface in interfaces)
-                    {
-                        // Register the service with its interface
-                        service.AddScoped(@interface, type);
-                    }
-                }
-            }
+            //foreach (var type in typeof(CommonHelperService).Assembly.GetTypes())
+            //{
+            //    if (type.Name.EndsWith("Service") && type.IsClass && !type.IsAbstract)
+            //    {
+            //        // Get all interfaces implemented by the type
+            //        var interfaces = type.GetInterfaces();
+            //        foreach (var @interface in interfaces)
+            //        {
+            //            // Register the service with its interface
+            //            service.AddScoped(@interface, type);
+            //        }
+            //    }
+            //}
 
         }
     }

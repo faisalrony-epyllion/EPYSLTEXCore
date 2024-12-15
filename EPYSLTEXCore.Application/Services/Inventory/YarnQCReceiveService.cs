fa8ec0cbb33f8fa582ee310febe74b121ade33ef
Swarojani-Dhar <swarojanidhar@epylliongroup.com>
2024-12-15 09:28:58 +0600

    Commit

--- EPYSLTEXCore.Application/Services/Inventory/YarnQCReceiveService.cs
+++ EPYSLTEXCore.Application/Services/Inventory/YarnQCReceiveService.cs
@@ -379,7 +379,7 @@ namespace EPYSLTEXCore.Application.Services.Inventory.Yarn
                 switch (entity.EntityState)
                 {
                     case EntityState.Added:
-                        entity.QCReceiveMasterID = await _service.GetMaxIdAsync(TableNames.YARN_QC_RECEIVE_MASTER);
+                        entity.QCReceiveMasterID = await _service.GetMaxIdAsync(TableNames.YARN_QC_RECEIVE_MASTER, RepeatAfterEnum.NoRepeat, transactionGmt, _connectionGmt);
                         entity.QCReceiveNo = await _service.GetMaxNoAsync(TableNames.YARN_QC_RECEIVE_NO, 1, RepeatAfterEnum.NoRepeat, "00000", transactionGmt, _connectionGmt);
 
                         maxChildId = await _service.GetMaxIdAsync(TableNames.YARN_QC_RECEIVE_CHILD, entity.YarnQCReceiveChilds.Count, RepeatAfterEnum.NoRepeat, transactionGmt, _connectionGmt);

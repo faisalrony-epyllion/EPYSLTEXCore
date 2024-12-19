f6c3de15ffc2579dd0c9e165c6cff6b6dfa8dda8
Md. Faisal Rony <faisal.rony@epylliongroup.com>
2024-12-19 12:32:08 +0600

    Menu Load By User Code

--- EPYSLTEXCore.API/Contollers/Menu/MenuAPIController.cs
+++ EPYSLTEXCore.API/Contollers/Menu/MenuAPIController.cs
@@ -2,16 +2,18 @@
 using EPYSLTEXCore.API.Contollers.APIBaseController;
 using EPYSLTEXCore.API.Contollers.ReportAPI;
 using EPYSLTEXCore.Application.Interfaces;
+using EPYSLTEXCore.Infrastructure.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
+using System.Data.Entity;
 
 namespace EPYSLTEXCore.API.Contollers.Menu
 {
     //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    public class MenuAPIController : Controller
+    public class MenuAPIController : ApiBaseController
     {
         private readonly IMenuService _IMenuService;
         private readonly ILogger<ReportAPIController> _logger;
@@ -19,7 +21,7 @@ namespace EPYSLTEXCore.API.Contollers.Menu
         private readonly IUserService _userService;
         private static readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1, 1); //// For restric same cache key access multiple user at a time
 
-        public MenuAPIController(IMenuService IMenuService, ILogger<ReportAPIController> logger, IMemoryCache cache,IUserService userService)
+        public MenuAPIController(IMenuService IMenuService, ILogger<ReportAPIController> logger, IMemoryCache cache,IUserService userService) : base(userService)
         {
             this._IMenuService = IMenuService;
             this._logger = logger;
@@ -32,16 +34,17 @@ namespace EPYSLTEXCore.API.Contollers.Menu
         {
             //var UserInfo = AppUser;
             //var lst = await _IMenuService.GetMenusAsync(UserInfo.UserCode, applicationId, UserInfo.CompanyId);// for report project
-            var lst = await _IMenuService.GetMenusAsync(1, applicationId, 2);
+            var lst = await _IMenuService.GetMenusAsync(AppUser.UserCode, applicationId, 2);
             return Ok(lst);
         }
+        
 
         [HttpGet("GetAllMenuReport/{applicationId}")]
         public async Task<IActionResult> GetAllMenuReport(int applicationId)
         {
             //var UserInfo = AppUser;
             //var lst = await _IMenuService.GetMenusAsync(UserInfo.UserCode, applicationId, UserInfo.CompanyId);// for report project
-            var lst = await _IMenuService.GetAllMenuReport(1, applicationId, 2);
+            var lst = await _IMenuService.GetAllMenuReport(AppUser.UserCode, applicationId, 2);
             return Ok(lst);
         }
     }

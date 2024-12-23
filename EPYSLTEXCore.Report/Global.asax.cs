725802c13c2d94ae487b2c5eb3d07b2d25ea5204
Nishadur Rahman Evan <nishad@epylliongroup.com>
2024-12-23 12:10:53 +0600

    report-session-sharing

--- EPYSLTEXCore.Report/Global.asax.cs
+++ EPYSLTEXCore.Report/Global.asax.cs
@@ -2,6 +2,8 @@ using Autofac;
 using Autofac.Integration.Mvc;
 using EPYSLTEXCore.Report.Repositories;
 using EPYSLTEXCore.Report.Service;
+using System.Web;
+using System;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -35,7 +37,21 @@ namespace EPYSLTEXCore.Report
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-          
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*"); // Or specify a specific origin
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+            // Handle OPTIONS request (preflight request)
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.StatusCode = 200;
+                HttpContext.Current.Response.End(); // End the request
+            }
         }
     }
 }

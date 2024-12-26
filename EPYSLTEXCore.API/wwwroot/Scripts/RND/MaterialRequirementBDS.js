85595c2822c9bbe26cec31e3d6c4d95db0b4706a
SaifEnamB <shifuddin@epylliongroup.com>
2024-12-26 18:08:13 +0600

    Bad Request error message

--- EPYSLTEXCore.API/wwwroot/Scripts/RND/MaterialRequirementBDS.js
+++ EPYSLTEXCore.API/wwwroot/Scripts/RND/MaterialRequirementBDS.js
@@ -2731,7 +2731,16 @@
                 //masterData.CompositionList.unshift({ id: response.data.Id, text: response.data.SegmentValue });
                 // initChildTable(masterData.Childs);
             })
-            .catch(showResponseError)
+            .catch(error => {
+                if (error.response) {
+                    toastr.error(error.response.data);
+                } else {
+                    toastr.error('Error message:', error.response.data.Message);
+                }
+                args.cancel = true;
+            });
+
+            //.catch(showResponseError)
     }
     /*
     function saveComposition() {

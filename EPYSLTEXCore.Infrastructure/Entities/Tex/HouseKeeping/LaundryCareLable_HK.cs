9cd6c1e39f2715c3aa23cf4094b4afe251d74e83
Nishadur Rahman Evan <nishad@epylliongroup.com>
2024-12-28 11:06:31 +0600

    model_added

---
+++ EPYSLTEXCore.Infrastructure/Entities/Tex/HouseKeeping/LaundryCareLable_HK.cs
@@ -0,0 +1,26 @@
+﻿using Dapper.Contrib.Extensions;
+using EPYSLTEXCore.Infrastructure.Data;
+
+namespace EPYSLTEXCore.Infrastructure.Entities.Tex.HouseKeeping
+{
+    [Table("LaundryCareLable_HK")]
+    public class LaundryCareLable_HK : DapperBaseEntity
+    {
+        #region Table Properties
+        [ExplicitKey]
+        public int LCareLableID { get; set; }
+        public string CareType { get; set; }
+        public string CareName { get; set; }
+        public string ImagePath { get; set; }
+        public int SeqNo { get; set; }
+        #endregion
+
+        #region Additional Properties
+        [Write(false)]
+        public override bool IsModified => EntityState == System.Data.Entity.EntityState.Modified || LCareLableID > 0;
+        #endregion
+        public LaundryCareLable_HK()
+        {
+        }
+    }
+}

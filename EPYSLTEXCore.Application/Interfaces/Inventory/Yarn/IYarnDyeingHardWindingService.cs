7b28ea331a1d5a095877985b9661e8e303ecdc09
ashiq-epyllion <ashiqur.rahman@epylliongroup.com>
2025-01-01 17:36:33 +0600

    .

---
+++ EPYSLTEXCore.Application/Interfaces/Inventory/Yarn/IYarnDyeingHardWindingService.cs
@@ -0,0 +1,16 @@
+﻿using EPYSLTEXCore.Infrastructure.Entities;
+using EPYSLTEXCore.Infrastructure.Entities.Tex.Inventory.Yarn;
+using EPYSLTEXCore.Infrastructure.Statics;
+
+namespace EPYSLTEXCore.Application.Interfaces.Inventory.Yarn
+{
+    public interface IYarnDyeingHardWindingService
+    {
+        Task<List<HardWindingMaster>> GetPagedAsync(Status status, string pageName, PaginationInfo paginationInfo);
+        Task<HardWindingMaster> GetNewAsync(int YDBookingMasterID, int YDBatchID);
+        Task<HardWindingMaster> GetAsync(int id);
+        Task<HardWindingMaster> GetAllAsync(int id);
+        Task SaveAsync(HardWindingMaster entity);
+        Task UpdateEntityAsync(HardWindingMaster entity);
+    }
+}
